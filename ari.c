3dbaa9d1f9d13c877df432226ffab264ba3f4df9
SatyanshuVishwakarma <84718007+SatyanshuVishwakarma@users.noreply.github.com>
2021-06-30 11:53:12 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -25,32 +25,46 @@ string ari(string s){
 	{
 	case 1:
 		return "Kindergarten";
+		break;
 	case 2:
 		return "First/Second Grade";
+		break;
 	case 3:
 		return "Third Grade";
+		break;
 	case 4:
 		return "Fourth Grade";
+		break;
 	case 5:
 		return "Fifth Grade";
+		break;
 	case 6:
 		return "Sixth Grade";
+		break;
 	case 7:
 		return "Seventh Grade";
+		break;
 	case 8:
 		return "Eighth Grade";
+		break;
 	case 9:
 		return "Ninth Grade";
+		break;
 	case 10:
 		return "Tenth Grade";
+		break;
 	case 11:
 		return "Eleventh Grade";
+		break;
 	case 12:
 		return "Twelfth grade";
+		break;
 	case 13:
 		return "College student";
+		break;
 	case 14:
 		return "Professor";
+		break;
 	default:
 		break;
 	}
